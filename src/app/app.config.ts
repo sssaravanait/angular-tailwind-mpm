7b0191a873f2ba26c5c0ccff01a3234e0f3aa5b0
saravanakumar.ss <saravanakumar.ss@dsrc.co.in>
2026-09-24 11:27:27 +0530

    primeng setup initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes),
+        providePrimeNG({
+            ripple: true,
+            theme: {
+                preset: Aura,
+                options: {
+                    prefix: 'p',
+                    darkModeSelector: 'system',
+                    cssLayer: false,
+                    cssVariables: true
+                }
+            }
+        })
+    ]
 };
--- src/app/features/customers/customers-list/customers-list.ts
+++ src/app/features/customers/customers-list/customers-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { AutoCompleteModule } from 'primeng/autocomplete';
 @Component({
-    imports: [],
+    imports: [AutoCompleteModule],
     selector: 'app-customers-list',
     styleUrl: './customers-list.css',
-    templateUrl: './customers-list.html',
+    // templateUrl: './customers-list.html',
+    template: `<p-autocomplete />`,
 })
 export class CustomersList {}
